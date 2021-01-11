315f476b583759b4de485afc093da120e2111e1d
edkrueger <edkrueger@gmail.com>
2021-01-10 18:21:20 -0600

    Clean up.

--- tests/test_math.py
+++ tests/test_math.py
@@ -2,19 +2,9 @@
 Tests math.py.
 """
 from resc.math import (
-    log_ceil_rec,
     recursive_integer_multiplication,
     karatsuba_multiplication,
-    recursive_integer_multiplication_prepadded,
 )
-
-
-def test_log_ceil_rec():
-    """Tests log_cel_rec."""
-    assert log_ceil_rec(32, 2) == 5
-    assert log_ceil_rec(5, 2) == 3
-    assert log_ceil_rec(9, 3) == 2
-    assert log_ceil_rec(10, 3) == 3
 
 
 def test_recursive_integer_multiplication():
@@ -33,9 +23,3 @@ def test_karatsuba_multiplication():
     assert karatsuba_multiplication(123, 456) == 123 * 456
     assert karatsuba_multiplication(12, 3456) == 12 * 3456
     assert karatsuba_multiplication(3456, 0) == 0
-
-
-def test_recursive_integer_multiplication_prepadded():
-    """Tests test_recursive_integer_multiplication_prepadded."""
-    assert recursive_integer_multiplication_prepadded(3, 9) == 3 * 9
-    assert recursive_integer_multiplication_prepadded(123, 456) == 123 * 456
